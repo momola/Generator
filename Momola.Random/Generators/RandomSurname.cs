60b02fe243f9bc53f2a948f24743a2c1e9eabf2a
Justyna Momola <j.momola@gmail.com>
2015-04-12 21:32:11 +0200

    randomSurname

---
+++ Momola.Random/Generators/RandomSurname.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Momola.Random.Generators
+{
+    class RandomSurname
+    {
+         private int countValues;
+        private System.Random myRandom;
+        private int randValue;
+        private string returnSurname;
+        private List<string> surnameList;
+
+        public RandomSurname()
+        {
+          myRandom = new System.Random();
+          surnameList=new List<string>();
+          surnameList.Add("Nowak");
+          surnameList.Add("Kowalski");
+          surnameList.Add("Malysz");
+         
+        }
+
+
+
+        public string GetRandom()
+        {
+         countValues =surnameList.Count;
+         randValue = myRandom.Next(countValues);
+         returnSurname = surnameList[randValue];
+         return returnSurname;
+            
+        }
+    }
+}
