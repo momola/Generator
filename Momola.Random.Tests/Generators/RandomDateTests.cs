4a72dc0403336eed1d1104f6b2025e7f846ab68f
Justyna Momola <j.momola@gmail.com>
2015-05-07 13:48:23 +0200

    RandomDate and RandomDateTests

---
+++ Momola.Random.Tests/Generators/RandomDateTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Momola.Random.Abstraction;
+using Momola.Random.Generators;
+using NUnit.Framework;
+
+namespace Momola.Random.Tests.Generators
+{
+    [TestFixture]
+    class RandomDateTests
+    {
+            [Test]
+            public void GetRandomShouldReturnNotNullOrEmptyDate()
+            {   //Arrange
+
+                //Act 
+                IRandomDate dateGen = new RandomDate();
+                DateTime result = dateGen.GetRandom();
+
+                //Assert
+                Assert.IsNullOrEmpty(result.ToShortDateString(), "GetRandom should return not empty datetime");
+            }
+         [Test]
+            public void GetRandomShouldReturnNotEmptyDateBetweenDates(DateTime startTime, DateTime endTime)
+            {
+                //Arrange
+
+                //Act 
+                IRandomDate dateGen = new RandomDate();
+                DateTime result = dateGen.GetRandom(startTime, endTime);
+
+                //Assert
+                Assert.IsNotNull(result, "Get random should return not null value.");
+                Assert.IsNotEmpty(result.ToShortDateString(), "Get random should return not empty or white space value");
+            }
+         [Test]
+            public void GetRandomShouldReturnNotEmptyDateWithYear(int year)
+            {
+                //Arrange
+
+                //Act 
+                IRandomDate dateGen = new RandomDate();
+                DateTime result = dateGen.GetRandom(year);
+
+                //Assert
+                Assert.IsNotNull(result, "Get random should return not null value.");
+                Assert.IsNotEmpty(result.ToShortDateString(), "Get random should return not empty or white space value");
+            }
+         [Test]
+            public void GetRandomShouldReturnNotEmptyDateWithYearAndMonth(int year, int month)
+            {
+                //Arrange
+
+                //Act 
+                IRandomDate dateGen = new RandomDate();
+                DateTime result = dateGen.GetRandom(year, month);
+
+                //Assert
+                Assert.IsNotNull(result, "Get random should return not null value.");
+                Assert.IsNotEmpty(result.ToShortDateString(), "Get random should return not empty or white space value");
+            }
+
+
+
+
+
+
+    }
+}
---
+++ Momola.Random/Generators/RandomDate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Momola.Random.Abstraction;
+
+namespace Momola.Random.Generators
+{
+   public class RandomDate: IRandomDate
+    {
+        private System.Random MyRandom= new System.Random();
+        public DateTime myDate= new DateTime();
+        public TimeSpan mySpan = new TimeSpan();
+        private int howDays;
+        private int randValue;
+
+        
+        public DateTime GetRandom()
+        {
+           randValue= MyRandom.Next(10000);
+           myDate = myDate.Date;
+           myDate = myDate.AddDays(-randValue);
+           return myDate;
+
+        }
+
+        public DateTime GetRandom(DateTime startDate, DateTime endDate)
+        {
+            mySpan = startDate - endDate;
+            howDays=mySpan.Days;
+            randValue= MyRandom.Next(howDays);
+            myDate= startDate.AddDays(randValue);
+            return myDate;
+        }
+
+        public DateTime GetRandom(int year)
+        {
+            myDate = myDate.AddYears(year);
+            howDays=myDate.DayOfYear;
+            randValue = MyRandom.Next(howDays);
+            myDate= myDate.AddDays(1);
+            myDate= myDate.AddMonths(1);
+            myDate= myDate.AddDays(randValue);
+            return myDate;
+        }
+
+        public DateTime GetRandom(int year, int month)
+        {
+            myDate = myDate.AddYears(year);
+            myDate = myDate.AddMonths(month);
+            howDays = myDate.Month;
+            randValue = MyRandom.Next(howDays);
+            myDate = myDate.AddDays(1);
+            myDate = myDate.AddDays(randValue);
+            return myDate;
+        }
+    }
+}
